c4edc4083ca6faeb06957d451fd6045f96bb9f06
wiebetimmers <82147541+wiebetimmers@users.noreply.github.com>
2021-11-16 11:59:08 +0100

    Add files via upload

--- stoch_sim_ass1.py
+++ stoch_sim_ass1.py
@@ -3,6 +3,7 @@ import random
 from PIL import Image, ImageDraw
 from tqdm import tqdm
 import numpy as np
+import math
 import os
 import matplotlib.pyplot as plt
 import multiprocessing
@@ -13,7 +14,7 @@ fig = plt.figure(figsize=(8,6))
 # https://www.fractalus.com/kerry/articles/area/mandelbrot-area.html , even checken of we dit wel mogen gebruiken
 
 # Set seed for reproducibility
-np.random.seed(420)
+#np.random.seed(420)
 
 #test
 print('test git')
@@ -28,7 +29,7 @@ def mandelbrot(c, max_iter):
     return n
 
 
-def monte_carlo(max_iter):
+def monte_carlo():
     sample_area_count = 0
     for i in range(par.NO_SAMPLES):
 
@@ -40,9 +41,9 @@ def monte_carlo(max_iter):
         c = complex(par.RE_START + x * (par.RE_END - par.RE_START),
                     par.IM_START + y * (par.IM_END - par.IM_START))
         # Compute the number of iterations
-        m = mandelbrot(c, max_iter)
+        m = mandelbrot(c, par.MAX_ITER)
         # The color depends on the number of iterations
-        color = 255 - int(m * 255 / max_iter)
+        color = 255 - int(m * 255 / par.MAX_ITER)
         if color == 0:
             sample_area_count += 1
 
@@ -52,33 +53,101 @@ def monte_carlo(max_iter):
     return area_approx
 
 
-def latin_hypercube(max_iter):
+def improved_monte_carlo(): # https://www.researchgate.net/publication/228755472_Strategies_for_Improving_the_Efficiency_of_Monte-Carlo_Methods
     sample_area_count = 0
-    x_C = []
-    y_C = []
-    x_range = (par.RE_END - par.RE_START)
-    y_range = (par.IM_END - par.IM_START)
-    box_size_x = x_range / par.NO_SAMPLES
-    box_size_y = y_range / par.NO_SAMPLES
-    lowerbounds_x = np.arange(par.RE_START, par.RE_END, box_size_x)
-    lowerbounds_y = np.arange(par.IM_START, par.IM_END, box_size_y)
-    np.random.shuffle(lowerbounds_x)
-    np.random.shuffle(lowerbounds_y)
+    for i in range(par.NO_SAMPLES):
 
-    for i in tqdm(range(par.NO_SAMPLES)):
-        rand_x_idx = random.choice(range(0, len(lowerbounds_x)))
-        rand_y_idx = random.choice(range(0, len(lowerbounds_y)))
-        x = np.random.uniform(low=lowerbounds_x[rand_x_idx], high=lowerbounds_x[rand_x_idx]+box_size_x)
-        y = np.random.uniform(low=lowerbounds_y[rand_y_idx], high=lowerbounds_y[rand_x_idx]+box_size_y)
+        # Draw random numbers from uniform distribution
+        x = np.random.uniform(low=0, high=1)
+        y = np.random.uniform(low=0, high=1)
 
-        lowerbounds_x = np.delete(lowerbounds_x, rand_x_idx)
-        lowerbounds_y = np.delete(lowerbounds_y, rand_y_idx)
-
-        c = complex(x, y)
-         # Compute the number of iterations
-        m = mandelbrot(c, max_iter)
+        # Convert coordinates to complex number and compute the mandelbrot iterations
+        c = complex(par.RE_START + x * (par.RE_END - par.RE_START),
+                    par.IM_START + y * (par.IM_END - par.IM_START))
+        # Compute the number of iterations
+        m = mandelbrot(c, par.MAX_ITER)
         # The color depends on the number of iterations
-        color = 255 - int(m * 255 / max_iter)
+        color = 255 - int(m * 255 / par.MAX_ITER)
+        if color == 0:
+            sample_area_count += 1
+
+    # Calculate average of the samples and determine area approx
+    within_mb_set = sample_area_count / par.NO_SAMPLES
+    area_approx = within_mb_set * par.AREA_SQUARE
+    return area_approx
+
+
+def orthogonal_sampling():
+    major = par.NO_SUBSPACES
+    samples = major*major
+    sample_area_count = 0
+    m = 0
+    xlist = np.empty((major, major))
+    ylist = np.empty((major, major))
+    scale_x = (abs(par.RE_START) + abs(par.RE_END)) / samples
+    scale_y = (abs(par.IM_START) + abs(par.IM_END)) / samples
+    print(scale_x, scale_y)
+
+    for i in tqdm(range(0, major)):
+        for j in range(0, major):
+            xlist[i][j] = ylist[i][j] = m
+            m += 1
+
+    np.random.shuffle(xlist)
+    np.random.shuffle(ylist)
+
+    for i in tqdm(range(0, major)):
+        for j in range(0, major):
+            x = par.RE_START + (scale_x * (xlist[i][j]+ np.random.uniform(0,1)))
+            y = par.IM_START + (scale_y * (ylist[j][i]+ np.random.uniform(0,1)))
+
+            c = complex(x, y)
+            # Compute the number of iterations
+            m = mandelbrot(c, par.MAX_ITER)
+            # The color depends on the number of iterations
+            color = 255 - int(m * 255 / par.MAX_ITER)
+            if color == 0:
+                sample_area_count += 1
+                plt.plot(x, y, 'ro', color='green', ms=72. / fig.dpi)
+            else:
+                plt.plot(x, y, 'ro', color='red', ms=72. / fig.dpi)
+            #plt.axvline(x=(par.RE_START + (scale_x * (xlist[i][j]))), color='black')
+            #plt.axhline(y= (par.IM_START + (scale_y * (ylist[j][i]))), color='black')
+
+    plt.show()
+    # Calculate average of the samples and determine area approx
+    within_mb_set = sample_area_count / par.NO_SAMPLES
+    area_approx = within_mb_set * par.AREA_SQUARE
+    return area_approx
+
+
+def latin_hypercube():
+    sample_area_count = 0
+    m = 0
+    xlist = np.zeros(par.NO_SAMPLES)
+    ylist = np.zeros(par.NO_SAMPLES)
+    scale_x = (abs(par.RE_START) + abs(par.RE_END)) / par.NO_SAMPLES
+    scale_y = (abs(par.IM_START) + abs(par.IM_END)) / par.NO_SAMPLES
+
+    for i in tqdm(range(0, par.NO_SAMPLES)):
+        xlist[i] = ylist[i] = m
+        m += 1
+
+    np.random.shuffle(xlist)
+    np.random.shuffle(ylist)
+
+    for i in tqdm(xlist):
+        x = par.RE_START + scale_x * (xlist[int(i)] + np.random.uniform(0, 1))
+        y_index = int(np.random.randint(0, len(ylist)))
+        y = par.IM_START + scale_y * (ylist[y_index] + np.random.uniform(0, 1))
+        #plt.axvline(x=(par.RE_START + (scale_x * (xlist[int(i)]))), color='black')
+        #plt.axhline(y=(par.IM_START + (scale_y * (ylist[y_index]))), color='black')
+        ylist = np.delete(ylist, y_index)
+        c = complex(x, y)
+        # Compute the number of iterations
+        m = mandelbrot(c, par.MAX_ITER)
+        # The color depends on the number of iterations
+        color = 255 - int(m * 255 / par.MAX_ITER)
         if color == 0:
             sample_area_count += 1
             plt.plot(x, y, 'ro', color='green', ms=72. / fig.dpi)
@@ -92,10 +161,33 @@ def latin_hypercube(max_iter):
     return area_approx
 
 
+def pure_random_sampling():
+    sample_area_count = 0
+    for i in range(par.NO_SAMPLES):
+
+        # Draw random numbers from uniform distribution
+        x = np.random.uniform(low=par.RE_START, high=par.RE_END)
+        y = np.random.uniform(low=par.IM_START, high=par.IM_END)
+
+        # Convert coordinates to complex number and compute the mandelbrot iterations
+        c = complex(x,y)
+        # Compute the number of iterations
+        m = mandelbrot(c, par.MAX_ITER)
+        # The color depends on the number of iterations
+        color = 255 - int(m * 255 / par.MAX_ITER)
+        if color == 0:
+            sample_area_count += 1
+
+    # Calculate average of the samples and determine area approx
+    within_mb_set = sample_area_count / par.NO_SAMPLES
+    area_approx = within_mb_set * par.AREA_SQUARE
+    return area_approx
+
+
 def balanced_is(process_range):
     areas_js = []
     areas_diff = []
-    area_is = monte_carlo(par.MAX_ITER)
+    area_is = monte_carlo()
     for j in tqdm(process_range): # van 1 tot 200 max iteraties
         area_js = monte_carlo(j)
         areas_js.append(area_js)
@@ -106,10 +198,9 @@ def balanced_is(process_range):
 
 def balanced_sample_size(sizes):
     areas_js = []
-    areas_diff = []
     for j in tqdm(sizes): # van 1 tot 200 max iteraties
         par.NO_SAMPLES = j
-        area_js = monte_carlo(par.MAX_ITER)
+        area_js = monte_carlo()
         areas_js.append(area_js)
     return areas_js
 
@@ -137,8 +228,9 @@ def plot_sample_sizes(ss, ssz):
     plt.savefig('Diff_samplesizes_area_%s.jpg' % (par.MAX_ITER))
     return
 
-
-print(latin_hypercube(par.MAX_ITER))
+#print(latin_hypercube())
+#print(orthogonal_sampling())
+#print(pure_random_sampling())
 
 '''
 if __name__ == '__main__':
